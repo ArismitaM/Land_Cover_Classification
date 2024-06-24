6fa09537a2318b7d49f48ac9829edfc5bb0a1872
Arismita Mukherjee <146760434+ArismitaM@users.noreply.github.com>
2024-06-24 16:47:35 +0530

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.cluster import DBSCAN
 
-img = rasterio.open('../data/archive/label/val/monrovia_17.tif')
+img = rasterio.open('../data/archive/label/tarin/aachen_63.tif')
 
 # To find the number of bands in the image
 num_bands = img.count
@@ -21,7 +21,7 @@ for i in range(1, num_bands + 1):
         band_idxOfj = np.asarray(np.where(img_band == j))
         band_idxOfj_flipped = np.transpose(band_idxOfj)
 
-        db_idxOfj_flipped = DBSCAN(eps=4, min_samples=20).fit(band_idxOfj_flipped)
+        db_idxOfj_flipped = DBSCAN(eps=4, min_samples=45).fit(band_idxOfj_flipped)
         labels_idxOfj_flipped = db_idxOfj_flipped.labels_
         n_clusters_idxOfj_flipped = len(set(labels_idxOfj_flipped)) - (1 if -1 in labels_idxOfj_flipped else 0)
         n_noise_idxOfj_flipped = list(labels_idxOfj_flipped).count(-1)
@@ -29,17 +29,19 @@ for i in range(1, num_bands + 1):
         print("Estimated number of clusters:", n_clusters_idxOfj_flipped)
         print("Estimated number of noise points:", n_noise_idxOfj_flipped)
 
-        show(band_matrixj)
+        plt.imshow(band_matrixj, origin='lower')
+        #plt.show()
+        #show(band_matrixj)
 
         # Hot vector plot (Unflipped)
-        plt.title(f"Hot vector plot UnFlipped Pixel value: {j}")
-        plt.scatter(band_idxOfj[1, :], band_idxOfj[0, :])
-        plt.show()
+        #plt.title(f"Hot vector plot UnFlipped Pixel value: {j}")
+        #plt.scatter(band_idxOfj[1, :], band_idxOfj[0, :])
+        #plt.show()
 
         # Hot vector plot (Flipped)
-        plt.title(f"Hot vector plot Flipped Pixel value: {j}")
-        plt.scatter(band_idxOfj_flipped[:, 1], band_idxOfj_flipped[:, 0])  # Swap x and y coordinates
-        plt.show()
+        #plt.title(f"Hot vector plot Flipped Pixel value: {j}")
+        #plt.scatter(band_idxOfj_flipped[:, 1], band_idxOfj_flipped[:, 0])  # Swap x and y coordinates
+        #plt.show()
 
 
         # Cluster plot
